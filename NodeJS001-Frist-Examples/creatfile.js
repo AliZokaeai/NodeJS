0c316dcdd3af0c6876f1dc447074c8e098b25ca1
AliZokaeai <alizokaeaidehaghani@gmail.com>
2024-02-01 19:42:54 +0330

    learning more about Node.js

--- NodeJS001-Frist-Examples/creatfile.js
+++
@@ -1,7 +0,0 @@
-var fs = require ('fs');
-// create a file name mynewfile.txt:
-
-fs.appendFile('munewfile.txt','I love you Nilaroz', function(err){
-    if(err) throw err;
-    console.log('Saved!');
-})
