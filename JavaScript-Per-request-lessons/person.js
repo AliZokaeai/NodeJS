678d87417a6c69a6129527009074fcb41f96ac64
AliZokaeai <alizokaeaidehaghani@gmail.com>
2024-02-09 20:00:08 +0330

    Read untl the ENd of JavaScript

---
+++ JavaScript-Per-request-lessons/person.js
@@ -0,0 +1,13 @@
+const name = "Ali";
+const age = 40;
+
+export {name, age};
+
+// const message = () =>{
+//     const name = "ali";
+//     const age = 40;
+//     return name + ' is '+ age + 'Year old.';
+
+// };
+
+// export default message;
